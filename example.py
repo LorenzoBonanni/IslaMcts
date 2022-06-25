b9e8029b053a7a7da214df8ba35ee0881f273022
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2022-06-25 20:06:59 +0200

    finished mcts implementation

--- example.py
+++ example.py
@@ -5,15 +5,15 @@ import gym
 from action_selection_functions import ucb1
 from mcts import Mcts
 
-real_env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=True)
-observation, info = real_env.reset(seed=42, return_info=True)
+real_env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False)
+observation = real_env.reset(seed=42, return_info=False)
 sim_env = copy(real_env)
 done = False
 
 while not done:
-    agent = Mcts(2, 0.7, 200, observation, sim_env, ucb1, 15)
+    agent = Mcts(2, 10, observation, sim_env, ucb1, 15)
     action = agent.fit()
-    observation, reward, done, info = real_env.step(action)
+    observation, reward, done, _ = real_env.step(action)
     real_env.render()
 
 real_env.close()
--- mcts.py
+++ mcts.py
@@ -3,68 +3,86 @@ from copy import copy
 import numpy as np
 from gym import Env
 
-from action_selection_functions import ucb1
-
 
 class Mcts:
-    def __init__(self, C: float, gamma: float, n_sim: int, root_data, env: Env, action_selection_fn, max_depth: int):
+    def __init__(self, C: float, n_sim: int, root_data, env: Env, action_selection_fn, max_depth: int):
         self.C = C
-        self.gamma = gamma
         self.n_sim = n_sim
         self.max_depth = max_depth
-        self.env = env
+        self.env = copy(env)
         self.action_selection_fn = action_selection_fn
-        self.root = StateNode(root_data, env, C, gamma)
+        self.root = StateNode(root_data, env, C, self.action_selection_fn)
 
     def fit(self):
-        action = 0
-        current = self.root
-        depth = 0
-        child = 1
         for s in range(self.n_sim):
-            curr_env = copy(self.env)
-            # selection
-            while child:
-                action = self.action_selection_fn(current.total, self.C, current.visit_actions, current.n)
-                child = current.actions.get(action, None)
-            observation, _, _, _ = curr_env.step(action)
-            state = StateNode(observation, curr_env, self.C, self.gamma)
-            # expansion
-            current.actions[action] = {state}
+            self.root.build_tree(self.max_depth)
+
+        return self.action_selection_fn(self.root.total, self.C, self.root.visit_actions, self.root.n)
+
+
+class ActionNode:
+    def __init__(self, data, env, C, action_selection_fn):
+        self.data = data
+        self.env = copy(env)
+        self.total = 0
+        self.n = 0
+        self.C = C
+        self.children = {}
+        # self.visit_children = {}
+        self.action_selection_fn = action_selection_fn
+
+    def build_tree(self, max_depth) -> float:
+        observation, _, _, _ = self.env.step(self.data)
+        state = self.children.get(observation, None)
+        if state is None:
+            state = StateNode(observation, self.env, self.C, self.action_selection_fn)
+            self.children[observation] = state
+            # TODO set rollout max depth
             # rollout
-            reward = state.rollout(self.max_depth)
-            current.total += reward
-            current.n += 1
-            current.visit_actions[action] += 1
-            # TODO backpropagate
-            # reset child to a value
-            child = 1
+            reward = state.rollout(max_depth)
+        else:
+            # rollout
+            reward = state.build_tree(max_depth)
+
+        # backpropagation
+        self.total += reward
+        self.n += 1
+        state.n += 1
+        state.total += reward
+        return reward
 
 
 class StateNode:
-    def __init__(self, data, env, C, gamma):
+    def __init__(self, data, env, C, action_selection_fn):
         self.data = data
-        self.env = env
+        self.env = copy(env)
         self.total = 0
         self.n = 0
         self.visit_actions = np.zeros(env.action_space.n)
         self.actions = {}
         self.C = C
-        self.gamma = gamma
+        self.action_selection_fn = action_selection_fn
 
     def rollout(self, max_depth) -> float:
-        environment = copy(self.env)
+        curr_env = copy(self.env)
         done = False
         reward = 0
         depth = 0
-        while not done or depth < max_depth:
-            _, reward, done, _ = environment.step(environment.action_space.sample())
+        while not done and depth < max_depth:
+            _, reward, done, _ = curr_env.step(curr_env.action_space.sample())
             depth += 1
         return reward
 
-    def expand_action(self):
-        pass
-
-    def tree_traversal(self):
-        action = ucb1(self.total, self.C, self.visit_actions, self.n)
-        observation, reward, done, info = self.env.step(action)
+    def build_tree(self, max_depth):
+        curr_env = copy(self.env)
+        # selection
+        action = self.action_selection_fn(self.total, self.C, self.visit_actions, self.n)
+        child = self.actions.get(action, None)
+        if child is None:
+            child = ActionNode(action, curr_env, self.C, self.action_selection_fn)
+            self.actions[action] = child
+        # rollout + backpropagation
+        reward = child.build_tree(max_depth)
+        self.n += 1
+        self.visit_actions[action] += 1
+        return reward
