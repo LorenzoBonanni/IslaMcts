4f862af1d83e2031ebd52faa3f984ae7dc360c0b
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2022-07-08 15:14:38 +0200

    compleated agent factory

--- AgentFactory.py
+++ AgentFactory.py
@@ -1,8 +1,11 @@
 from collections.abc import Hashable
 
 from agents.mcts import Mcts
-from agents.mcts_continuous import MctsHashStateContinuous
+from agents.mcts_continuous import MctsContinuous
+from agents.mcts_continuous_hash import MctsContinuousHash
 from agents.mcts_hash import MctsHash
+from agents.mcts_state_progressive_widening import MctsStateProgressiveWidening
+from agents.mcts_state_progressive_widening_hash import MctsStateProgressiveWideningHash
 
 
 class AgentFactory:
@@ -19,8 +22,11 @@ class AgentFactory:
                 return MctsHash(*kwargs)
         elif agent_type == "continuous":
             if hashable:
-                # return MctsHashStateContinuous(*kwargs)
-                # TODO: return MctsContinuous Normal
-                pass
+                return MctsContinuous(*kwargs)
             else:
-                return MctsHashStateContinuous(*kwargs)
+                return MctsContinuousHash(*kwargs)
+        elif agent_type == "state_pw":
+            if hashable:
+                return MctsStateProgressiveWidening(*kwargs)
+            else:
+                return MctsStateProgressiveWideningHash(*kwargs)
