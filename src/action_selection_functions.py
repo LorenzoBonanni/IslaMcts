fixed ucb1 score calculation

src/action_selection_functions.py
@@ -2,18 +2,21 @@ import random
 
 import numpy as np
 
+from src.agents.mcts import StateNode
 
-def ucb1(total: float, C: float, visit_child: list, n_visits: int):
+
+def ucb1(node: StateNode):
     """
     computes the best action based on the ucb1 value
-    :param total: the total reward obtained by the node so far
-    :param C: exploration constant
-    :param visit_child: an array containing the number of visits of each action
-    :param n_visits: the number of visits of the node
-    :return:
     """
-    avg_reward = np.array(total) / len(visit_child)
-    ucb_score = avg_reward + C * np.sqrt(np.log(n_visits) / np.array(visit_child))
+    n_visits = node.ns
+    visit_child = []
+    node_values = []
+    for c in node.actions.values():
+        visit_child.append(c.na)
+        node_values.append(c.total/c.na)
+
+    ucb_score = np.array(node_values) + node.C * np.sqrt(np.log(n_visits) / np.array(visit_child))
 
     # to avoid biases we randomly choose between the actions which maximize the ucb1 score
     a = np.random.choice(np.flatnonzero(ucb_score == ucb_score.max()))
@@ -64,7 +67,7 @@ def grid_policy(prior_knowledge, n_actions):
         #
         # probs = knowledge_bias + probs
         # probs = probs / sum(probs)
-
+        #
         # sampled_action = random.choices(population=indices, weights=probs)[0]
 
         # to avoid biases if two or more actions have the same value we choose randomly between them

src/agents/mcts_action_progressive_widening_hash.py
@@ -94,16 +94,9 @@ class StateNodeProgressiveWideningHash(StateNodeHash):
             self.visit_actions[action_bytes] = 0
             self.actions[action_bytes] = child
         else:
-            action_index = self.action_selection_fn(self.total, self.C, list(self.visit_actions.values()), self.ns)
+            action_index = self.action_selection_fn(self)
             action_bytes = list(self.visit_actions.keys())[action_index]
             child = self.actions[action_bytes]
-        # if 0 in self.visit_actions.values():
-        #     # random action
-        #     index = np.random.choice(np.flatnonzero(self.visit_actions == 0))
-        #     a = list(self.visit_actions.keys())[index]
-        #     action = np.frombuffer(a, dtype=float)
-        # else:
-        #     action = self.action_selection_fn(self.total, self.C, self.visit_actions, self.ns)
 
         # in order to get instant_reward set the state into the environment to the current state
         self.env.__dict__[self.state_variable] = self.env.unwrapped.__dict__[self.state_variable] = self.data

src/example_rocket.py
@@ -3,12 +3,7 @@ import numpy as np
 from tqdm import tqdm
 
 from action_selection_functions import ucb1, discrete_default_policy
-from src.agent_factory import AgentFactory
 from src.agents.mcts_action_progressive_widening_hash import MctsActionProgressiveWideningHash
-from src.agents.mcts_continuous_hash import MctsContinuousHash
-from src.agents.mcts_double_progressive_widening_hash import MctsDoubleProgressiveWideningHash
-from src.agents.mcts_hash import MctsHash
-from src.agents.mcts_state_progressive_widening_hash import MctsStateProgressiveWideningHash
 
 time = np.arange(0, 0.4, 0.001)
 
@@ -55,21 +50,20 @@ def main():
     sim_env = gym.make('gym_goddard:Goddard-v0')
 
     observation = real_env.reset()
-    sim_env.reset()
-    # real_env.render()
     rollout_fn = discrete_default_policy(11)
 
     state_log = [observation]
     extra_log = []
-    agent = None
 
     for _ in tqdm(time):
         last_state = observation
+        sim_env.reset()
+        # APW 22min
         agent = MctsActionProgressiveWideningHash(
             root_data=observation,
             env=sim_env.unwrapped,
             n_sim=1000,
-            C=0,
+            C=0.0005,
             action_selection_fn=ucb1,
             gamma=1,
             rollout_selection_fn=rollout_fn,
@@ -84,16 +78,9 @@ def main():
         observation, reward, done, extra = real_env.step(action)
         state_log.append(observation)
         extra_log.append(list(extra.values()))
-        # print()
-        # print(f"S: {last_state} A: {action}, S': {observation}, R: {reward}")
-        agent.visualize()
-        break
-        # real_env.render()
     extra_log[-1] = extra_log[-1][:-1]
-    generate_plots(real_env, state_log, extra_log)
-
-    # TODO make comparison graphs
-
+    state_log = np.array(state_log)
+    extra_log = np.array(extra_log)
 
 if __name__ == '__main__':
     main()