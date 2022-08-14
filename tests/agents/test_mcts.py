9ad2a33e56e285d9417ce0c4d8a8b1de2d83ef8e
Lorenzo Bonanni <lorenzopuntobonanni@gmail.com>
2022-08-14 15:26:01 +0200

    added test for StateNode

--- tests/agents/test_mcts.py
+++ tests/agents/test_mcts.py
@@ -1,10 +1,11 @@
 from unittest import TestCase, mock
-from unittest.mock import Mock
+from unittest.mock import Mock, patch, MagicMock
 
+import numpy as np
 from parametrize import parametrize
 
 import islaMcts.utils as utils
-from islaMcts.agents.mcts import Mcts
+from islaMcts.agents.mcts import Mcts, StateNode, ActionNode
 from islaMcts.agents.parameters.mcts_parameters import MctsParameters
 
 
@@ -56,7 +57,7 @@ class TestMcts(TestCase):
         result = self.test_class.fit()
 
         # THEN
-        self.assertEqual(my_deepcopy_mock.call_count, n_sim+1)
+        self.assertEqual(my_deepcopy_mock.call_count, n_sim + 1)
         self.assertEqual(result, 4)
 
     @mock.patch("builtins.max")
@@ -69,3 +70,56 @@ class TestMcts(TestCase):
         result = self.test_class.fit()
 
         self.assertIn(result, [2, 3])
+
+
+class TestStateNode(TestCase):
+    def setUp(self) -> None:
+        self.test_param = MctsParameters(
+            root_data=None,
+            env=None,
+            n_sim=None,
+            C=None,
+            action_selection_fn=None,
+            gamma=0.9,
+            rollout_selection_fn=None,
+            state_variable=None,
+            max_depth=None,
+            n_actions=1
+        )
+        self.test_class = StateNode(None, self.test_param)
+
+    @patch.object(ActionNode, 'build_tree')
+    def test_build_tree_zero_in_visit_action(self, mocked_build_tree):
+        # GIVEN
+        mocked_build_tree.return_value = 5
+
+        # WHEN
+        result = self.test_class.build_tree(10)
+
+        # THEN
+        self.assertEqual(list(self.test_class.actions.keys())[0], 0)
+        self.assertEqual(self.test_class.ns, 1)
+        self.assertEqual(self.test_class.visit_actions[0], 1)
+        self.assertEqual(self.test_class.total, 4.5)
+        self.assertEqual(self.test_class.actions[0].data, 0)
+        self.assertEqual(result, 5)
+
+    def test_build_tree_non_zero_in_visit_action(self):
+        # GIVEN
+        self.test_class.visit_actions = np.array([1, 1])
+        child = Mock(q_value=1, data=1)
+        acs = {
+            1: child
+        }
+        self.test_class.actions = acs
+        self.test_param.action_selection_fn = Mock(return_value=1)
+        child.build_tree = MagicMock(return_value=5)
+
+        # WHEN
+        result = self.test_class.build_tree(10)
+
+        # THEN
+        self.assertEqual(self.test_class.ns, 1)
+        self.assertEqual(self.test_class.visit_actions[1], 2)
+        self.assertEqual(self.test_class.total, 4.5)
+        self.assertEqual(result, 5)
